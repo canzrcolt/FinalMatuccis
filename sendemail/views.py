4fdaf79505d66cecce88b5d437fcdd8564487523
canzrcolt <canzrcolt@gmail.com>
2020-11-12 15:28:03 -0500

    adding https redirect

--- sendemail/views.py
+++ sendemail/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.http import HttpResponse
+from django.conf import settings
+from django.http import HttpResponseRedirect
 
+class SSLMiddleware(object):
+
+    def process_request(self, request):
+        if not any([settings.DEBUG, request.is_secure(), request.META.get("HTTP_X_FORWARDED_PROTO", "") == 'https']):
+            url = request.build_absolute_uri(request.get_full_path())
+            secure_url = url.replace("http://", "https://")
+            return HttpResponseRedirect(secure_url)
+			
 def successView(request):
     return render(request, "success.html", {})
 
