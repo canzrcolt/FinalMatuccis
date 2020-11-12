8bfb68124bfc2aa19182ea281818bab7f316074a
canzrcolt <canzrcolt@gmail.com>
2020-11-12 17:31:21 -0500

    trying to redirect

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
